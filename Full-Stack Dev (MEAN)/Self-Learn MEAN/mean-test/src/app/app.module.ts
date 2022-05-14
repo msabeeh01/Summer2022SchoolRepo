c545391290aa7b9dd6de9ddc098b4df234cc03e0
Sabeeh Muhammad <muhammadsabeeh01@gmail.com>
2022-05-13 23:16:02 -0400

    meAn tutorial

---
+++ Full-Stack Dev (MEAN)/Self-Learn MEAN/mean-test/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
