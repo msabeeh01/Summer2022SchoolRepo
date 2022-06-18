8cb0900685431e96acbad8ea96080eb5e502b4c1
Sabeeh Muhammad <muhammadsabeeh01@gmail.com>
2022-06-17 21:15:50 -0400

    Assignment 2

--- Full-Stack Dev (MEAN)/Week 5 Express CRUD, Passport.JS/config/db.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //which database we are connecting to
-    "URI": "mongodb://localhost/book_store"
-}
--- Full-Stack Dev (MEAN)/Week 5 Express CRUD, Passport.JS/public/Scripts/app.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    function start(){
-        console.log("App Started...");
-    }
-
-    window.addEventListener("load", start);
-})();
